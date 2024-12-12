d48a1b06d65f45fe0c21fbe8c9473f4d0066dea8
Steven Pangestu <stevenpangestu02@gmail.com>
2024-12-12 07:58:55 +0700

    commit

--- src/View/MainMenu.java
+++ src/View/MainMenu.java
@@ -1,5 +1,17 @@
 package View;
 
+import javax.swing.JFrame;
+
 public class MainMenu {
 
+    public MainMenu() {
+        showMainMenu();
+    }
+
+    public void showMainMenu() {
+        JFrame frame = new JFrame();
+        frame.setBounds(50, 50, 350, 350);
+        frame.setVisible(true);
+        frame.setLayout(null);
+    }
 }
